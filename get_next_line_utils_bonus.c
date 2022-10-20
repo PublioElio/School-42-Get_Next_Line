9cc49fae8191efe811036fd4d1c35d08257c6b47
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-20 16:03:31 +0200

    Add files via upload

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -61,9 +61,9 @@ char	*ft_strjoin(char *s1, char *s2)
 	i = -1;
 	c = 0;
 	if (s1)
-		while (s1[++i] != '\0')
+		while (s1[++i])
 			str[i] = s1[i];
-	while (s2[c] != '\0')
+	while (s2[c])
 		str[i++] = s2[c++];
 	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
 	free(s1);
